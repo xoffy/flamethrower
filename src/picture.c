42dcc1a7233e2837c94a8336fb6d2346be6d85dd
Valery Timiriliyev <timiriliyev@gmail.com>
2018-07-02 22:02:25 +0900

    Removed YCbCr support, now only RGBA

--- src/picture.c
+++ src/picture.c
@@ -21,146 +21,97 @@ unsigned char clamp_comp(int comp) {
     return comp;
 }
 
-RGBPicture *rgb_picture_new(void) {
-    RGBPicture *rgb;
+RGBAPicture *picture_create(void) {
+    RGBAPicture *rgba;
 
-    u_debug("rgb_picture_new()...");
+    u_debug("picture_create()...");
     
-    rgb = (RGBPicture *)calloc(1, sizeof(RGBPicture));
-    if (!rgb) {
+    rgba = malloc(sizeof(RGBAPicture));
+    if (!rgba) {
         u_error("Can't allocate memory for RGBPicture struct!");
         return NULL;
     }
     
-    u_debug("-- 0x%X", rgb);
-    
-    return rgb;
+    u_debug(">> 0x%X", rgba);
+    return rgba;
 }
 
-RGBPicture *rgb_picture_new_dummy(int width, int height, int components) {
-    RGBPicture *rgb;
-    int n;
+RGBAPicture *picture_new(int width, int height) {
+    RGBAPicture *rgba;
+    int size;
     
-    u_debug("rgb_picture_new_dummy(%d, %d, %d)...", width, height, components);
+    u_debug("picture_new(%d, %d)...", width, height);
     
-    n = width * height * components;
-    rgb = rgb_picture_new();
-    if (!rgb) {
+    size = width * height * RGBA_COMPONENTS;
+    rgba = picture_create();
+    if (!rgba) {
         return NULL;
     }
-    rgb->width = width;
-    rgb->height = height;
-    rgb->components = components;
-    rgb->data = (unsigned char *)malloc(sizeof(unsigned char) * n);
-    if (!rgb->data) {
-        u_error("Can't allocate memory for RGBPicture data!");
+    rgba->width = width;
+    rgba->height = height;
+    rgba->data = malloc(sizeof(unsigned char) * size);
+    if (!rgba->data) {
+        u_error("Can't allocate memory for RGBAPicture data!");
         return NULL;
     }
-    memset(rgb->data, 128, n);
+    memset(rgba->data, 128, size);
     
-    return rgb;
+    return rgba;
 }
 
-RGBPicture *rgb_picture_load(const char *path) {
-    RGBPicture *rgb;
+RGBAPicture *picture_load(const char *path) {
+    RGBAPicture *rgba;
 
-    u_debug("rgb_picture_load(%s)...", path);
+    u_debug("picture_load(%s)...", path);
 
-    rgb = rgb_picture_new();
-    if (!rgb) {
+    rgba = picture_create();
+    if (!rgba) {
         return NULL;
     }
-    rgb->data = stbi_load(path, &rgb->width, &rgb->height, &rgb->components, 0);
-    if (!rgb->data) {
+    rgba->data = stbi_load(path, &rgba->width, &rgba->height,
+        NULL, RGBA_COMPONENTS);
+    if (!rgba->data) {
         u_error("Can't read %s!", path);
         return NULL;
     }
-    
-    return rgb;
+
+    return rgba;
 }
 
-#define RGB_DEFAULT_COMPONENTS 3
+void picture_delete(RGBAPicture *rgba) {
+    u_debug("picture_delete(0x%X)...", rgba);
 
-RGBPicture *rgb_picture_from_ycbcr(const YCbCrPicture *ycbcr) {
-    RGBPicture *rgb;
-    int n, i;
-
-    u_debug("rgb_picture_from_ycbcr(0x%X)...", ycbcr);
-
-    n = ycbcr->width * ycbcr->height * RGB_DEFAULT_COMPONENTS;
-    rgb = rgb_picture_new();
-    if (!rgb) {
-        return NULL;
+    if (rgba->data) {
+        stbi_image_free(rgba->data);
     }
-    rgb->data = (unsigned char *)malloc(sizeof(unsigned char) * n);
-    if (!rgb->data) {
-        u_error("Can't allocate memory for RGBPicture data!");
-        return NULL;
-    }
-    rgb->width = ycbcr->width;
-    rgb->height = ycbcr->height;
-    rgb->components = RGB_DEFAULT_COMPONENTS;
-    
-    i = 0;
-    
-    while (i < n) {
-        rgb->data[i + 0] = clamp_comp(0.0
-            + (298.082 * ycbcr->data[i + 0] / 256.0)
-            + (408.583 * ycbcr->data[i + 2] / 256.0)
-            - 222.921);
-        rgb->data[i + 1] = clamp_comp(0.0
-            + (298.082 * ycbcr->data[i + 0] / 256.0)
-            - (100.291 * ycbcr->data[i + 1] / 256.0)
-            - (208.120 * ycbcr->data[i + 2] / 256.0)
-            + 135.576);
-        rgb->data[i + 2] = clamp_comp(0.0
-            + (298.082 * ycbcr->data[i + 0] / 256.0)
-            + (516.412 * ycbcr->data[i + 1] / 256.0)
-            - 276.836);
-        
-        i += YCBCR_COMPONENTS;
-    }
-    
-    return rgb;
+    free(rgba);
 }
-
-void rgb_picture_delete(RGBPicture *rgb) {
-    u_debug("rgb_picture_delete(0x%X)...", rgb);
-
-    if (rgb->data) {
-        stbi_image_free(rgb->data);
-    }
-    free(rgb);
-    rgb = NULL;
-}
-
-
 
 #define JPEG_QUALITY    95
 #define PNG_STRIDE      0
 
-int rgb_picture_write(const RGBPicture *rgb, const char *path) {
+int picture_write(const RGBAPicture *rgba, const char *path) {
     const char *ext;
     int rc = 0;
     
-    u_debug("rgb_picture_write(0x%X, %s)...", rgb, path);
+    u_debug("picture_write(0x%X, %s)...", rgba, path);
     
     ext = u_get_file_ext(path);
     
     if (!ext) {
         u_error("Please provide output extension!");
     } else if (strcmp(ext, "jpg") == 0 || strcmp(ext, "jpeg") == 0) {
-        rc = stbi_write_jpg(path, rgb->width, rgb->height,
-            rgb->components, rgb->data, JPEG_QUALITY);
+        rc = stbi_write_jpg(path, rgba->width, rgba->height,
+            RGBA_COMPONENTS, rgba->data, JPEG_QUALITY);
     } else if (strcmp(ext, "png") == 0) {
-        rc = stbi_write_png(path, rgb->width, rgb->height,
-            rgb->components, rgb->data, PNG_STRIDE);
+        rc = stbi_write_png(path, rgba->width, rgba->height,
+            RGBA_COMPONENTS, rgba->data, PNG_STRIDE);
     } else if (strcmp(ext, "bmp") == 0) {
-        rc = stbi_write_bmp(path, rgb->width, rgb->height,
-            rgb->components, rgb->data);
+        rc = stbi_write_bmp(path, rgba->width, rgba->height,
+            RGBA_COMPONENTS, rgba->data);
     } else if (strcmp(ext, "tga") == 0) {
-        rc = stbi_write_tga(path, rgb->width, rgb->height,
-            rgb->components, rgb->data);
+        rc = stbi_write_tga(path, rgba->width, rgba->height,
+            RGBA_COMPONENTS, rgba->data);
     } else {
         u_error("Unknown output extension %s!", ext);
     }
@@ -168,220 +119,43 @@ int rgb_picture_write(const RGBPicture *rgb, const char *path) {
     return rc;
 }
 
-void rgb_picture_resize(RGBPicture *rgb, int nw, int nh) {
+void picture_resize(RGBAPicture *rgba, int new_width, int new_height) {
     unsigned char *output;
 
-    output = malloc(sizeof(unsigned char) * nw * nh * rgb->components);
+    u_debug("picture_resize(0x%X, %d, %d)...", rgba, new_width, new_height);
 
+    output = malloc(sizeof(unsigned char)
+        * new_width * new_height * RGBA_COMPONENTS);
     stbir_resize_uint8(
-        rgb->data, rgb->width, rgb->height, 0,
-        output, nw, nh, 0, rgb->components
+        rgba->data, rgba->width, rgba->height, 0,
+        output, new_width, new_height, 0, RGBA_COMPONENTS
     );
-    free(rgb->data);
+    free(rgba->data);
     
-    rgb->data = output;
-    rgb->width = nw;
-    rgb->height = nh;
+    rgba->data = output;
+    rgba->width = new_width;
+    rgba->height = new_height;
 }
 
-
-YCbCrPicture *ycbcr_picture_new(void) {
-    YCbCrPicture *ycbcr;
+/*
+rgb->data[i + 0] = clamp_comp(0.0
+    + (298.082 * ycbcr->data[i + 0] / 256.0)
+    + (408.583 * ycbcr->data[i + 2] / 256.0)
+    - 222.921);
+rgb->data[i + 1] = clamp_comp(0.0
+    + (298.082 * ycbcr->data[i + 0] / 256.0)
+    - (100.291 * ycbcr->data[i + 1] / 256.0)
+    - (208.120 * ycbcr->data[i + 2] / 256.0)
+    + 135.576);
+rgb->data[i + 2] = clamp_comp(0.0
+    + (298.082 * ycbcr->data[i + 0] / 256.0)
+    + (516.412 * ycbcr->data[i + 1] / 256.0)
+    - 276.836);
     
-    u_debug("ycbcr_picture_new()...");
-    
-    ycbcr = (YCbCrPicture *)calloc(1, sizeof(YCbCrPicture));
-    if (!ycbcr) {
-        u_error("Can't allocate memory for YCbCrPicture struct!");
-        return NULL;
-    }
-    
-    u_debug("-- 0x%X", ycbcr);
-    
-    return ycbcr;
-}
-
-
-
-void ycbcr_picture_reset(YCbCrPicture *ycbcr) {
-    int n = ycbcr->width * ycbcr->height * YCBCR_COMPONENTS;
-    memset(ycbcr->data, 128, n);
-}
-
-YCbCrPicture *ycbcr_picture_dummy(int width, int height) {
-    YCbCrPicture *ycbcr;
-    int n;
-    
-    u_debug("ycbcr_picture_dummy(%d, %d)...", width, height);
-    
-    n = width * height * YCBCR_COMPONENTS;
-    ycbcr = ycbcr_picture_new();
-    if (!ycbcr) {
-        return NULL;
-    }
-    ycbcr->width = width;
-    ycbcr->height = height;
-    ycbcr->data = (unsigned char *)malloc(sizeof(unsigned char) * n);
-    if (!ycbcr->data) {
-        u_error("Can't allocate memory for YCbCrPicture data!");
-        return NULL;
-    }
-    memset(ycbcr->data, 128, n);
-    
-    return ycbcr;
-}
-
-YCbCrPicture *ycbcr_picture_from_rgb(const RGBPicture *rgb) {
-    YCbCrPicture *ycbcr;
-    int n, y, r;
-
-    u_debug("ycbcr_picture_from_rgb(0x%X)...", rgb);
-
-    if (!rgb) {
-        return NULL;
-    }
-
-    n = rgb->width * rgb->height * YCBCR_COMPONENTS;
-    ycbcr = ycbcr_picture_new();
-    if (!ycbcr) {
-        return NULL;
-    }
-    ycbcr->data = (unsigned char *)malloc(sizeof(unsigned char) * n);
-    if (!ycbcr->data) {
-        u_error("Can't allocate memory for YCbCrPicture data!");
-        return NULL;
-    }
-    ycbcr->width = rgb->width;
-    ycbcr->height = rgb->height;
-    
-    y = r = 0;
-    
-    while (y < n) {
-        ycbcr->data[y + 0] = clamp_comp(16.0
-            + (65.7380 * rgb->data[r + 0] / 256.0)
-            + (129.057 * rgb->data[r + 1] / 256.0)
-            + (25.0640 * rgb->data[r + 2] / 256.0));
-        ycbcr->data[y + 1] = clamp_comp(128.0
-            - (37.9450 * rgb->data[r + 0] / 256.0)
-            - (74.4940 * rgb->data[r + 1] / 256.0)
-            + (112.439 * rgb->data[r + 2] / 256.0));
-        ycbcr->data[y + 2] = clamp_comp(128.0
-            + (112.439 * rgb->data[r + 0] / 256.0)
-            - (94.1540 * rgb->data[r + 1] / 256.0)
-            - (18.2850 * rgb->data[r + 2] / 256.0));
-        
-        y += YCBCR_COMPONENTS;
-        r += rgb->components;
-    }
-    
-    return ycbcr;
-}
-
-#define PIXEL(p, x, y, c) (p->width * (c) * (y) + (c) * (x))
-
-unsigned char *ycbcr_picture_get_pixel(const YCbCrPicture *ycbcr, int x, int y)
-{ 
-    return ycbcr->data + (ycbcr->width * YCBCR_COMPONENTS * y
-        + YCBCR_COMPONENTS * x);
-}
-
-void ycbcr_picture_scan(YCbCrPicture *ycbcr,
-    int (*func)(YCbCrPicture *, int x, int y, unsigned char *))
-{
-    int x, y, rc;
-    
-    u_debug("ycbcr_picture_scan(0x%X)...", ycbcr);
-    
-    for (y = 0; y < ycbcr->height; y++) {
-        for (x = 0; x < ycbcr->width; x++) {
-            rc = func(ycbcr, x, y, ycbcr_picture_get_pixel(ycbcr, x, y));
-        }
-    }
-}
-
-YCbCrPicture *ycbcr_picture_copy(YCbCrPicture *orig) {
-    YCbCrPicture *ycbcr;
-    int n;
-    
-    ycbcr = ycbcr_picture_new();
-    if (!ycbcr) {
-        return NULL;
-    }
-    
-    ycbcr->width = orig->width;
-    ycbcr->height = orig->height;
-    n = ycbcr->width * ycbcr->height * 3;
-    ycbcr->data = (unsigned char *)malloc(sizeof(unsigned char) * n);
-    if (!ycbcr->data) {
-        u_error("Can't allocate memory for YCbCrPicture data!");
-        return NULL;
-    }
-    memcpy(ycbcr->data, orig->data, n);
-    
-    return ycbcr;
-}
-
-void ycbcr_picture_merge(YCbCrPicture *ycbcr, YCbCrPicture *add) {
-    int x, y, max_w, max_h, p;
-    
-    u_debug("ycbcr_picture_merge(0x%X, 0x%X)", ycbcr, add);
-    
-    max_w = ycbcr->width <= add->width ? ycbcr->width : add->width;
-    max_h = ycbcr->height <= add->height ? ycbcr->height : add->height;
-    
-    for (y = 0; y < max_h; y++) {
-        for (x = 0; x < max_w; x++) {
-            p = PIXEL(ycbcr, x, y, YCBCR_COMPONENTS);
-            ycbcr->data[p + 0] = clamp_comp(ycbcr->data[p + 0]
-                + add->data[p + 0] - 128);
-            ycbcr->data[p + 1] = clamp_comp(ycbcr->data[p + 1]
-                + add->data[p + 1] - 128);
-            ycbcr->data[p + 2] = clamp_comp(ycbcr->data[p + 2]
-                + add->data[p + 2] - 128);
-        }
-    }
-}
-
-void ycbcr_picture_delete(YCbCrPicture *ycbcr) {
-    u_debug("ycbcr_picture_delete(0x%X)...", ycbcr);
-
-    if (ycbcr->data) {
-        free(ycbcr->data);
-    }
-    free(ycbcr);
-    ycbcr = NULL;
-}
-
-YCbCrPicture *ycbcr_picture_brdg_load(const char *path) {
-    RGBPicture *bridge;
-    YCbCrPicture *ycbcr;
-    
-    bridge = rgb_picture_load(path);
-    ycbcr = ycbcr_picture_from_rgb(bridge);
-    rgb_picture_delete(bridge);
-    
-    return ycbcr;
-}
-
-void ycbcr_picture_brdg_write(YCbCrPicture *ycbcr, const char *path) {
-    RGBPicture *bridge;
-    
-    bridge = rgb_picture_from_ycbcr(ycbcr);
-    rgb_picture_write(bridge, path);
-    rgb_picture_delete(bridge);
-}
-
-void ycbcr_picture_brdg_resize(YCbCrPicture **ycbcr, int nw, int nh) {
-    RGBPicture *bridge;
-
-    u_debug("ycbcr_picture_brdg_resize(0x%X, %d, %d)...", ycbcr, nw, nh);
-
-    bridge = rgb_picture_from_ycbcr(*ycbcr);
-    ycbcr_picture_delete(*ycbcr);
-    rgb_picture_resize(bridge, nw, nh);
-    *ycbcr = ycbcr_picture_from_rgb(bridge);
-    rgb_picture_delete(bridge);
-}
-
-
-
+ycbcr->data[p + 0] = clamp_comp(ycbcr->data[p + 0]
+    + add->data[p + 0] - 128);
+ycbcr->data[p + 1] = clamp_comp(ycbcr->data[p + 1]
+    + add->data[p + 1] - 128);
+ycbcr->data[p + 2] = clamp_comp(ycbcr->data[p + 2]
+    + add->data[p + 2] - 128);
+*/
--- src/picture.h
+++ src/picture.h
@@ -2,50 +2,23 @@
 #ifndef __PICTURE_H_
 #define __PICTURE_H_
 
-struct _RGBPicture {
+typedef struct {
     unsigned char   *data;
     int             width;
     int             height;
-    int             components;
-};
+} RGBAPicture;
 
-struct _YCbCrPicture {
-    unsigned char   *data;
-    int             width;
-    int             height;
-    int             components;
-};
-
-typedef struct _RGBPicture RGBPicture;
-typedef struct _YCbCrPicture YCbCrPicture;
+#define RGBA_COMPONENTS 4
+#define COLOR_CLAMP(x) ((x) < 0 ? 0 : ((x) > 255 ? 255 : 0))
 
 unsigned char clamp_comp(int comp);
 
-RGBPicture *rgb_picture_new(void);
-RGBPicture *rgb_picture_new_dummy(int width, int height, int components);
-RGBPicture *rgb_picture_load(const char *path);
-RGBPicture *rgb_picture_from_ycbcr(const YCbCrPicture *ycbcr);
-void rgb_picture_delete(RGBPicture *rgb);
-int rgb_picture_write(const RGBPicture *rgb, const char *path);
-void rgb_picture_resize(RGBPicture *rgb, int nw, int nh);
+RGBAPicture *picture_new(int width, int height);
+RGBAPicture *picture_load(const char *path);
+void picture_delete(RGBAPicture *rgb);
 
-YCbCrPicture *ycbcr_picture_new(void);
-YCbCrPicture *ycbcr_picture_dummy(int width, int height);
-YCbCrPicture *ycbcr_picture_from_rgb(const RGBPicture *rgb);
-void ycbcr_picture_delete(YCbCrPicture *ycbcr);
-unsigned char *ycbcr_picture_get_pixel(const YCbCrPicture *ycbcr, int x, int y);
-void ycbcr_picture_scan(YCbCrPicture *ycbcr,
-    int (*func)(YCbCrPicture *, int x, int y, unsigned char *));
-void ycbcr_picture_merge(YCbCrPicture *ycbcr, YCbCrPicture *add);
-YCbCrPicture *ycbcr_picture_copy(YCbCrPicture *orig);
-
-YCbCrPicture *ycbcr_picture_brdg_load(const char *path);
-void ycbcr_picture_brdg_write(YCbCrPicture *ycbcr, const char *path);
-void ycbcr_picture_brdg_resize(YCbCrPicture **ycbcr, int nw, int nh);
-void ycbcr_picture_reset(YCbCrPicture *ycbcr);
-
-#define YCBCR_COMPONENTS    3
-
+int picture_save(const RGBAPicture *rgb, const char *path);
+void picture_resize(RGBAPicture *rgb, int new_width, int new_height);
 
 #endif
 
