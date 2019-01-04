cf8bd21daae5ebd92ad67842763685d896b686e0
Valery Timiriliyev <timiriliyev@gmail.com>
2019-01-05 03:21:07 +1100

    Add some kind of "visual tests".

---
+++ tests/README.md
@@ -0,0 +1,5 @@
+Put your pictures to `./pictures` and run `bulk-test.sh`.
+
+Resulting secamized pictures will be lying in `./secamized` directory.
+
+Note: `bulk-test.sh` assumes that `flamethrower` executable is in `/build/flamethrower`. Be sure to configure Meson to build to that directory.
---
+++ tests/bulk-test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SECAMIZER="../build/flamethrower"
+
+if [ ! -x "$SECAMIZER" ]; then
+  echo "Can't find \`flamethrower\` executable! Did you build it?"
+  exit 1
+fi
+
+[ -d "./secamized" ] || mkdir -p "./secamized"
+
+for i in ./pictures/*; do
+  echo "-- $i..."
+  $SECAMIZER $* $i "./secamized/$(basename $i)"
+done
+
+echo "-- Done!"
