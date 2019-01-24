1e410548a2a04c1460a9b47b3045417734538f21
Valery Timiriliyev <timiriliyev@gmail.com>
2019-01-24 12:58:15 +1100

    Add still frame video tests.

---
+++ tests/image2video.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+( cd "../build" && ninja )
+
+EXECUTABLE="../build/flamethrower"
+
+if [ ! -x "$EXECUTABLE" ]; then
+  echo "Can't find \`flamethrower\` executable! Is build failed?"
+  exit 1
+fi
+
+INPUT=${1:-pictures/police-duck.jpg}
+OUTPUT=$(basename $INPUT | sed 's?\.[^.]*??')
+LENGTH=${2:-10}
+
+[ -d "./experiments/videos" ] || mkdir -p "./experiments/videos"
+[ -f "./experiments/videos/$OUTPUT.mp4" ] && rm -f "./experiments/videos/$OUTPUT.mp4"
+
+$EXECUTABLE -R -f jpg -a $(($LENGTH * 25)) $INPUT -O \
+  | ffmpeg \
+  -f image2pipe -r 25 -c:v mjpeg -i - \
+  -f lavfi -i "sine=frequency=1000:duration=$LENGTH" \
+  -c:v libx264 -pix_fmt yuv420p -c:a aac ./image2video/$OUTPUT.mp4
